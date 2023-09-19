d2ce39d95e54767cced08a1f96fe9ca790d10428
Blin <ba51658@ubt-uni.net>
2023-09-19 11:20:26 +0200

    List layout and css styling

--- ToDoList/ClientApp/src/components/ToDoList.js
+++ ToDoList/ClientApp/src/components/ToDoList.js
@@ -10,6 +10,7 @@ const ToDoList = () => {
             </div>
 
             <div className="list-items">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
                 <div className="date"> Due date: </div>
                 <div className="list-body">
                     <li className="item">
@@ -17,7 +18,20 @@ const ToDoList = () => {
                             <input type="checkbox" />
                             <p>Item 1</p>
                         </div>
-                        <div>delete</div>
+                        <div className="buttons">
+                            <i class="bi bi-pen"></i>
+                            <i class="bi bi-trash"></i>
+                        </div>
+                    </li>
+                    <li className="item">
+                        <div>
+                            <input type="checkbox" />
+                            <p>Item 2</p>
+                        </div>
+                        <div className="buttons">
+                            <i class="bi bi-pen"></i>
+                            <i class="bi bi-trash"></i>
+                        </div>
                     </li>
                 </div>
             </div>
